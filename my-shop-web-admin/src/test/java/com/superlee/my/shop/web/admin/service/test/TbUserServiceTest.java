4df0b4c7862fdb29df1e24e2c470bf9d7a838cf7
lichao <825877206@qq.com>
2019-12-06 20:16:31 +0800

    添加静态资源文件

---
+++ my-shop-web-admin/src/test/java/com/superlee/my/shop/web/admin/service/test/TbUserServiceTest.java
@@ -0,0 +1,34 @@
+package com.superlee.my.shop.web.admin.service.test;
+
+import com.superlee.my.shop.domain.TbUser;
+import com.superlee.my.shop.web.admin.service.TbUserService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+/**
+ * @author : lichao892
+ * @date : 2019/12/6 19:09
+ * <p>
+ * 描述 :    TODO
+ */
+
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(locations = {"classpath:spring-context.xml", "classpath:spring-context-druid.xml", "classpath:spring-context-mybatis.xml", "classpath:spring-mvc.xml", "classpath:mybatis-config.xml"})
+//public class TbUserServiceTest {
+//
+//    @Autowired
+//    private TbUserService tbUserService;
+//
+//    @Test
+//    public void testSelectAll() {
+//        List<TbUser> tbUsers = tbUserService.selectAll();
+//        for (TbUser tbUser : tbUsers) {
+//            System.out.println(tbUser.getUsername());
+//        }
+//    }
+//}
