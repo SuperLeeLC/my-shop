b77ad5b05d181a83bf39d302e67056610d768fb8
lichao <825877206@qq.com>
2019-12-13 20:58:26 +0800

    修改 mysql地址 为 localhost

--- my-shop-web-admin/src/main/java/com/superlee/my/shop/web/admin/web/controller/MainController.java
+++ my-shop-web-admin/src/main/java/com/superlee/my/shop/web/admin/web/controller/MainController.java
@@ -1,8 +1,11 @@
 package com.superlee.my.shop.web.admin.web.controller;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServlet;
 
 /**
  * @author : lichao892
